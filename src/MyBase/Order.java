6cf7800572ea1ee305acb9fd7b02f9854c6b23f9
Willie Reed <william.reed@us.sogeti.com>
2015-01-14 07:28:31 -0800

    cleaning up code

--- src/MyBase/Order.java
+++ src/MyBase/Order.java
@@ -1,7 +1,6 @@
 package MyBase;
 
 public interface Order {
-    public int numShares = 0;
     public abstract void execute ( );
     public abstract String getSymbolName ( );
     public abstract int getSymbolQuantity ( );
